b86452a5ab33a27a5471c3a296bbcd995a14b037
sgargallo <sgargallo@gmail.com>
2022-10-10 12:50:36 +0200

    Update lr-fuse.sh

--- scriptmodules/libretrocores/lr-fuse.sh
+++ scriptmodules/libretrocores/lr-fuse.sh
@@ -14,7 +14,7 @@ rp_module_desc="ZX Spectrum emu - Fuse port for libretro"
 rp_module_help="ROM Extensions: .sna .szx .z80 .tap .tzx .gz .udi .mgt .img .trd .scl .dsk .zip\n\nCopy your ZX Spectrum games to $romdir/zxspectrum"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/fuse-libretro/master/LICENSE"
 rp_module_repo="git https://github.com/libretro/fuse-libretro.git master"
-rp_module_section="main"
+rp_module_section="main x86=opt"
 
 function sources_lr-fuse() {
     gitPullOrClone
